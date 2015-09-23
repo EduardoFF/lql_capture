0cba4de4b90d9be97034ab0f33efe8ffd0f84891
eduardo <eduardo@debian-t440p>
2015-09-24 00:42:52 +0200

    some bugs

--- capture/capture.cpp
+++ capture/capture.cpp
@@ -3,8 +3,16 @@
 #include "wifipcap/wifipcap.h"
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <strings.h>
 #include <set>
 #include <string>
+#include <sstream>
+#include <fstream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+//#include "protectedmutex.h"
+
 using namespace std;
 
 /// Cool templates
@@ -36,6 +44,8 @@ using namespace std;
 class TestCB : public WifipcapCallbacks
 {
 private:
+  int m_id;
+  double m_x,m_y;
   bool fcs_ok;
   int last_rssi;
 
@@ -48,6 +58,14 @@ private:
   std::set<uint64_t> nodes;
   std::map<std::string, int> cumm_rssi_per_ssid;
   std::map<std::string, int> n_rssi_per_ssid;
+  /*
+     * Mutex to control the access to the listAllNodes
+     */
+    pthread_mutex_t m_mutex;
+    /**
+     * Thread
+     */
+    pthread_t m_thread;
   
   typedef enum e_pType
     {
@@ -64,11 +82,14 @@ private:
     //    printf("%ld sec\n", t.tv_sec);
   }
 public:
-  TestCB()
+  TestCB(int id, double x, double y)
   {
     cumm_len = 0;
     next_update = 0;
     update_interval = 10;
+    m_id = id;
+    m_x = x;
+    m_y = y;
   }
 	/*
     void PacketBegin(const struct timeval& t, const u_char *pkt, int len, int origlen)
@@ -80,25 +101,121 @@ public:
 	cout << "}" << endl;
     }
 	*/
+
+  static void * UDPThreadEntryFunc(void * ptr) 
+  {
+    (( TestCB *) ptr)->UDPThreadFunc();
+    return NULL;
+  }
+
+  void updatePos(double x, double y)
+  {
+    pthread_mutex_lock(&m_mutex);
+    printf("got pos %.2f %.2f\n", x,y);
+    m_x = x;
+    m_y;
+    
+    pthread_mutex_unlock(&m_mutex);
+  }
+  void
+  UDPThreadFunc()
+  {
+       int sockfd,n;
+   struct sockaddr_in servaddr,cliaddr;
+   socklen_t len;
+   char mesg[1000];
+   double x,y;
+
+   sockfd=socket(AF_INET,SOCK_DGRAM,0);
+
+   bzero(&servaddr,sizeof(servaddr));
+   servaddr.sin_family = AF_INET;
+   servaddr.sin_addr.s_addr=htonl(INADDR_ANY);
+   servaddr.sin_port=htons(32000);
+   bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr));
+
+   for (;;)
+   {
+      len = sizeof(cliaddr);
+      n = recvfrom(sockfd,mesg,1000,0,(struct sockaddr *)&cliaddr,&len);
+      sendto(sockfd,mesg,n,0,(struct sockaddr *)&cliaddr,sizeof(cliaddr));
+      printf("-------------------------------------------------------\n");
+      mesg[n] = 0;
+      printf("Received the following:\n");
+      printf("%s",mesg);
+      printf("-------------------------------------------------------\n");
+
+      if( !n )
+	continue;
+      stringstream ss(mesg);
+      string cmd;
+      ss >> cmd;
+      if( cmd == "pos" )
+	{
+	  ss >> x >> y;
+      
+      
+	  updatePos(x,y);
+	}
+   }
+  }
+  
+  bool
+  runUDPServer()
+  {
+    //  printf("running\n");
+    pthread_mutex_init(&m_mutex, NULL);
+    int status = pthread_create(&m_thread, NULL, UDPThreadEntryFunc, this);
+    return (status == 0);
+  }
   void report(long int t)
   {
     int bw = cumm_len / update_interval;
     printf("===============================\n");
+    fflush(stdout);
     printf("report @ %ld\n", t);
-    printf("bw use %.4f MB/s\n", 1.0*bw/1e6);
+    printf("bw %.4f MB/s\n", 1.0*bw/1e6);
     printf("# aps %d\n", (int) aps.size());
     printf("# nodes %d\n", (int) nodes.size());
     printf("rssi_x_ssid:\n");
+    stringstream rssi_str;
+    int n_rssi =0;
     FOREACH(it, cumm_rssi_per_ssid )
       {
 	string ssid = it->first;
 	int rssi = it->second;
 	int n = n_rssi_per_ssid[ssid];
-	if( !n )
+	if( n < 5 )
 	  continue;
+	n_rssi++;
+	rssi_str << " " << ssid.c_str() << " " << rssi/n;
 	printf(" %s %d (%d)\n", ssid.c_str(), rssi/n, n);
       }
     printf("===============================\n\n");
+
+    stringstream fs;
+    fs << "/tmp/" << m_id << "_" << t <<".dat";
+    ofstream of(fs.str().c_str());
+    double x,y;
+    pthread_mutex_lock(&m_mutex);
+    x = m_x;
+    y = m_y;
+    pthread_mutex_unlock(&m_mutex);
+    of << m_id << " " << x << " " << y
+       << " " << 1.0*bw/1e6
+       << " " << (int) aps.size()
+       << " " << (int) nodes.size()
+       << " " << n_rssi << " " << rssi_str.str() << endl;
+    of.close();
+    stringstream cmd;
+    cmd << "scp -q " << fs.str() << " feo@195.176.38.35:lql/";
+    int ret = system(cmd.str().c_str());
+    //    printf("system returned %d\n", ret);
+    if( ret != 0 )
+      {
+	printf("NO_CONNECTION %ld %.2f %.2f\n", t, x,y);
+      }
+    
     reset();
     next_update = t + update_interval;
   }
@@ -144,7 +261,8 @@ public:
 
      void PacketBegin(const struct timeval& t, const u_char *pkt, int len, int origlen)
   {
-    // printf("packet BEGIN\n");
+    //     printf("packet BEGIN\n");
+    //fflush(stdout);
        last_ptype = NONE;
        last_tstamp = t.tv_sec;
        
@@ -153,7 +271,8 @@ public:
      }
      void PacketEnd()
   {
-    //       printf("packet END\n");
+    //     printf("packet END\n");
+    //  fflush(stdout);
        if( !next_update )
 	 {
 	   next_update = last_tstamp + update_interval;
@@ -169,6 +288,8 @@ public:
 
     void Handle80211DataFromAP(const struct timeval& t, const data_hdr_t *hdr, const u_char *rest, int len) 
     {
+      if( hdr == NULL)
+	return;
 	if (!fcs_ok) {
 	  //registerAP(hdr->sa.val);
 	  registerNode(hdr->da.val);
@@ -180,6 +301,8 @@ public:
     }
     void Handle80211DataToAP(const struct timeval& t, const data_hdr_t *hdr, const u_char *rest, int len) 
     {
+      if( hdr == NULL )
+	return;
 	if (!fcs_ok) {
 	  //registerAP(hdr->da.val);
 	  registerNode(hdr->sa.val);
@@ -209,14 +332,20 @@ public:
 
     void Handle80211MgmtBeacon(const struct timeval& t, const mgmt_header_t *hdr, const mgmt_body_t *body)
     {
+      if( hdr == NULL )
+	return;
 	if (!fcs_ok) {
 	  registerAP(hdr->sa.val);
 	    // cout << "  " << "802.11 mgmt:\t" 
 	    // 	 << hdr->sa << "\tbeacon\t\"" 
 	    // 	 << body->ssid.ssid << "\"" << endl;
-	    string ssid_str = (const char *)body->ssid.ssid;
-	    //	    printf("ssid %s\n", ssid_str.c_str());
-	    rssiSSID(ssid_str, last_rssi);
+	  if( body->ssid.length > 1 )
+	    {
+	      string ssid_str = (const char *)body->ssid.ssid;
+	      if( ssid_str.size() > 1)
+	      //printf("ssid %s\n", ssid_str.c_str());
+		rssiSSID(ssid_str, last_rssi);
+	    }
 	}
     }
 
@@ -250,6 +379,8 @@ public:
 	}
       void HandleRadiotap(const struct timeval& t, radiotap_hdr *hdr, const u_char *rest, int len)
   {
+    if( hdr == NULL)
+      return;
     if( hdr->has_signal_dbm )
       {
 	//	printf("rssi %d dbm\n ", hdr->signal_dbm);
@@ -261,6 +392,11 @@ public:
   }
 };
 
+void openSocketConn()
+{
+
+}
+
 /**
  * usage: test <pcap_trace_file>
  */
@@ -268,8 +404,24 @@ int main(int argc, char **argv)
 {
 
   
-  bool live = argc == 3 && atoi(argv[2]) == 1;
+  bool live = argc >= 3 && atoi(argv[2]) == 1;
     Wifipcap *wcap = new Wifipcap(argv[1], live);
-    wcap->Run(new TestCB());
+
+    int id=0;
+    double x,y;
+    x=y=0.0;
+    if( argc > 3)
+      {
+	id = atoi(argv[3]);
+      }
+    if( argc > 4)
+      {
+	x = atof(argv[4]);
+	y = atof(argv[5]);
+      }
+
+    TestCB *testCB = new TestCB(id,x,y);
+    testCB->runUDPServer();
+    wcap->Run(testCB);
     return 0;
 }
